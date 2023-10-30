1c4ec06e6ac1d5854e3a783c105d8c75c7932b6d
AnthemPython || Related <111581268+SeasonLeague@users.noreply.github.com>
2023-10-30 06:47:51 +0100

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Key": 'e5c039e6e7msh013425c48454853p157efbjsnebdbb788322f',
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
